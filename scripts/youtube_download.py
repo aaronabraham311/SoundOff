99f08f0707b96d434884e507b15c94987c705c6d
Aaron Abraham <aaronabraham311@gmail.com>
2019-12-19 13:01:11 -0700

    Finished download script; downloaded over 650 songs from different music eras

--- scripts/youtube_download.py
+++ scripts/youtube_download.py
@@ -1,12 +1,16 @@
 import os
 
-urls = {'70s_music': 'https://www.youtube.com/watch?v=N43mbd4qaHc&list=PLUcVRPQ0oeIqUZ_rMMg3FVxKl16g76CH5',
-        '80s_music': 'https://www.youtube.com/watch?v=bVey-358Vcc&list=PL85813B7453734CFB',
-        '90s_music': 'https://www.youtube.com/watch?v=m4oXDxPFT_s&list=PLcMd4zmqZWkDs_CMg1iDRgWePxn9_FXBw',
-        '00s_music': 'https://www.youtube.com/watch?v=SvAQAeLa-jY&list=PLW_aX2Vduw-sQTQDdmL7TdEiL_X8ldMyQ-',
-        '10s_music': 'https://www.youtube.com/watch?v=kn6-c223DUU&list=PL7Q2ZklqtR8B_EAUfXt5tAZkxhCApfFkL'}
+urls = {'70s_music': 'https://www.youtube.com/playlist?list=PLUcVRPQ0oeIqUZ_rMMg3FVxKl16g76CH5',
+        '80s_music': 'https://www.youtube.com/playlist?list=PL85813B7453734CFB',
+        '90s_music': 'https://www.youtube.com/playlist?list=PLcMd4zmqZWkDs_CMg1iDRgWePxn9_FXBw',
+        '00s_music': 'https://www.youtube.com/playlist?list=PL05E1623111A9A860',
+        '10s_music': 'https://www.youtube.com/playlist?list=PL7Q2ZklqtR8B_EAUfXt5tAZkxhCApfFkL'}
+
+os.chdir('music/english/')
 
 for directory, link in urls.items():
-    command = f"youtube-dl -o --playlist-end 100 ../music/english/{directory} {link}"
-    print(command)
-    #os.system("youtube-dl -o ")
+    print(os.curdir)
+    os.chdir(f"{directory}")
+    command = f"youtube-dl -i -f mp4 --yes-playlist {link}"
+    os.system(command)
+    os.chdir("../")
