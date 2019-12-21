591818e69317c6bc6ce1f8a6b7aebdc6a47b2a6d
Aaron Abraham <aaronabraham311@gmail.com>
2019-12-20 17:24:17 -0700

    Preprocessed YouTube music

--- scripts/youtube_download.py
+++ scripts/youtube_download.py
@@ -4,7 +4,7 @@ urls = {'70s_music': 'https://www.youtube.com/playlist?list=PLUcVRPQ0oeIqUZ_rMMg
         '80s_music': 'https://www.youtube.com/playlist?list=PL85813B7453734CFB',
         '90s_music': 'https://www.youtube.com/playlist?list=PLcMd4zmqZWkDs_CMg1iDRgWePxn9_FXBw',
         '00s_music': 'https://www.youtube.com/playlist?list=PL05E1623111A9A860',
-        '10s_music': 'https://www.youtube.com/playlist?list=PL7Q2ZklqtR8B_EAUfXt5tAZkxhCApfFkL'}
+        '10s-music': 'https://www.youtube.com/playlist?list=PL7Q2ZklqtR8B_EAUfXt5tAZkxhCApfFkL'}
 
 os.chdir('music/english/')
 
