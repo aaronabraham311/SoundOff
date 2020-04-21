7a8417125a57aa4a297b158bfc4dec9e173864a5
Aaron Abraham <aaronabraham311@gmail.com>
2020-04-21 16:32:29 -0600

    Updating url list and cutting out length of songs

--- scripts/youtube_download.py
+++ scripts/youtube_download.py
@@ -1,16 +1,52 @@
 import os
+import youtube_dl      
 
-urls = {'70s_music': 'https://www.youtube.com/playlist?list=PLUcVRPQ0oeIqUZ_rMMg3FVxKl16g76CH5',
-        '80s_music': 'https://www.youtube.com/playlist?list=PL85813B7453734CFB',
-        '90s_music': 'https://www.youtube.com/playlist?list=PLcMd4zmqZWkDs_CMg1iDRgWePxn9_FXBw',
-        '00s_music': 'https://www.youtube.com/playlist?list=PL05E1623111A9A860',
-        '10s-music': 'https://www.youtube.com/playlist?list=PL7Q2ZklqtR8B_EAUfXt5tAZkxhCApfFkL'}
+ydl_opts = {}
+ydl = youtube_dl.YoutubeDL(ydl_opts)
+
+'''
+70s music: 148 songs
+80s music: 184 songs
+90s music: 178 songs
+00s music: 144 songs
+10s music: 200 songs
+'''
+
+# Playlist of songs
+urls = {'1970s_music_videos': 'https://www.youtube.com/playlist?list=PLGBuKfnErZlAkaUUy57-mR97f8SBgMNHh',
+        '1980s_music_videos': 'https://www.youtube.com/playlist?list=PL90b28qTMGqIle4NmUvaAUtj20lAPETzE',
+        '1990s_music_videos': 'https://www.youtube.com/playlist?list=PLZyqOyXxaVETqpHhT_c5GPmAPzhJpJ5K7',
+        '2000s_music_videos': 'https://www.youtube.com/playlist?list=PLwW9XzYk5_cQyw-MDuD-bF-OCjJQ6UjZf',
+        '2010s-music_videos': 'https://www.youtube.com/playlist?list=PLeZgwVkN7bbfVLcqnz9l5RjASqUkBtpBe'}
 
 os.chdir('music/english/')
 
+
+# Downloading all songs in each playlist 
+
+# Downloading video and audio streams: https://unix.stackexchange.com/questions/230481/how-to-download-portion-of-video-with-youtube-dl-command
+# Getting video links from playlist: https://stackoverflow.com/questions/43910501/how-do-i-retrieve-individual-video-urls-from-a-playlist-using-youtube-dl-within
+
 for directory, link in urls.items():
-    print(os.curdir)
-    os.chdir(f"{directory}")
-    command = f"youtube-dl -i -f mp4 --yes-playlist {link}"
-    os.system(command)
-    os.chdir("../")
+
+    with ydl:
+        result = ydl.extract_info(link, download = False)
+
+        if 'entries' in result:
+            playlist = result['entries']
+
+            for i, item in enumerate(playlist):
+                video_url = result['entries'][i]['webpage_url']
+
+                print(os.curdir)
+                os.chdir(f"{directory}")
+                command = f"ffmpeg -ss 00:00:30.00 -i "OUTPUT-OF-FIRST URL" -t 00:00:30.00 -c copy out.mp4"
+                os.system(command)
+                os.chdir("../")
+
+
+
+# Looping through all songs and extracting 00:00:30 to 00:01:00 and putting into new directories
+for directory in urls.keys():
+    for filename in os.listdir(directory):
+        
